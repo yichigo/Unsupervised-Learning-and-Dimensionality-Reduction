8e109d3ebd5ecb92ea87f1bb1e8bad0247ba0300
yichigo <zycliamg@gmail.com>
2020-03-22 20:22:56 -0500

    Readme

---
+++ README.md
@@ -0,0 +1,76 @@
+# Unsupervised Learning and Dimensionality Reduction
+
+
+###  Program Language:
+	python3
+	
+### Packages:
+	pandas,
+	numpy,
+	matplotlib,
+	sklearn,
+	pickle,
+	plots.py (to plot roc curve)
+
+
+### input and output
+the input data and preprocessed data are saved in ./data/
+
+the output plots are exported to ./plots/
+
+
+### Running
+
+To run the codes, go to the folder ./src/,
+
+
+#### 1. Gestures Sensor Data Clustering and Dimensionality Reduction
+
+Data preprocessing, run: python gestures_data_preprocessing.py
+
+Clustering, open Jupyter Notebook, run: gestures.ipynb
+
+
+#### 2. Heart Disease Data Clustering and Dimensionality Reduction
+
+Data preprocessing, run: python heart_data_preprocessing.py
+
+Clustering, open Jupyter Notebook, run: heart.ipynb
+
+
+#### 3. Heart Disease Prediction
+
+##### 3.1 Original Neural Network
+
+run: python NN.py
+
+
+##### 3.2 Neural Network after Dimensionality Reduction ( without Cluster Label )
+
+run: 	python NN.py PCA
+
+		python NN.py ICA
+
+		python NN.py RP
+
+		python NN.py TSNE
+
+
+##### 3.2 Neural Network after Dimensionality Reduction ( add Cluster Label )
+
+run: 	python NN.py PCA KMeans
+
+		python NN.py PCA EM
+
+		python NN.py ICA KMeans
+
+		python NN.py ICA EM
+
+		python NN.py RP KMeans
+
+		python NN.py RP EM
+
+		python NN.py TSNE KMeans
+
+		python NN.py TSNE EM
+
--- src/plots.py
+++ src/plots.py
@@ -7,53 +7,6 @@ import os
 from sklearn.metrics import confusion_matrix, roc_curve, auc, precision_recall_curve
 from sklearn.utils.multiclass import unique_labels
 
-
-
-def plot_confusion_matrix(results, class_names, label_id, label_name):
-	PATH_OUTPUT = '../plots/'
-	image_path = os.path.join(PATH_OUTPUT, 'Confusion_Matrix_'+label_name+'.png')
-
-	y_true, y_pred = zip(*results)
-	y_true = np.array(y_true)
-	y_pred = np.array(y_pred)
-	y_true = y_true[:,label_id]
-	y_pred = y_pred[:,label_id]
-	# Compute confusion matrix
-	cm = confusion_matrix(y_true, y_pred)
-	cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-	# Only use the labels that appear in the data
-	#class_names = class_names[unique_labels(y_true, y_pred)]
-	
-	np.set_printoptions(precision=2)
-	fig, ax = plt.subplots()
-	im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
-	ax.figure.colorbar(im, ax=ax)
-	# We want to show all ticks...
-	ax.set(xticks=np.arange(cm.shape[1]), 
-		yticks=np.arange(cm.shape[0]),
-		# ... and label them with the respective list entries
-		xticklabels=class_names, yticklabels=class_names,
-		title='Normalized Confusion Matrix\n' + label_name,
-		ylabel='True',
-		xlabel='Predicted')
-
-	# Rotate the tick labels and set their alignment.
-	plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-		rotation_mode="anchor")
-
-	# Loop over data dimensions and create text annotations.
-	fmt = '.2f'
-	thresh = cm.max() / 2.
-	for i in range(cm.shape[0]):
-		for j in range(cm.shape[1]):
-			ax.text(j, i, format(cm[i, j], fmt), 
-				ha="center", va="center", 
-				color="white" if cm[i, j] > thresh else "black")
-	fig.tight_layout()
-
-	plt.savefig(image_path)
-
-
 def plot_roc(targets, probs, label_names):
 	PATH_OUTPUT = '../plots/'
 	fpr = dict()
